42ec3f9b2da13acde410c276ac13f2a253c38b93
Jahongir <jahongir.y.g@gmail.com>
2025-09-04 14:29:54 +0500

    deleted some no needed files

--- qr.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/skip2/go-qrcode"
-
-func qr() {
-	url := "http://localhost:8080/app" // ссылка на твой сервер
-	qrcode.WriteFile(url, qrcode.Medium, 256, "qrcode.png")
-}
